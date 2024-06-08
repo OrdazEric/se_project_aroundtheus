db900a9db6df2e94f5d36a44cc71c6af4ffed5b1
Eric Ordaz <ordaz.eric@gmail.com>
2024-06-08 07:54:26 -0700

    New updates for my project

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let firstcard = {name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+};
+
+let secondCard = {name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+};
+
+let thirdCard = {name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+};
+
+let fourthCard = {name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+};
+
+let fifthCard = {name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+};
+
+let sixthCard = {name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+};
+
+let initialCards = [firstcard, secondCard, thirdCard, fourthCard, fifthCard, sixthCard];
+console.log(initialCards);
